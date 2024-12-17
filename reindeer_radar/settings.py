fa26246e008778e6ba03b6996412c9f38fce5084
Taybe Muharem <taybemuharem@gmail.com>
2024-12-17 19:49:36 +0000

    Install, setup and migrate Allauth

--- reindeer_radar/settings.py
+++ reindeer_radar/settings.py
@@ -35,9 +35,9 @@ CLOUDINARY_URL = os.environ.get("CLOUDINARY_URL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-willwhyd-reindeerradar-4958ydmonhb.ws.codeinstitute-ide.net', 'reindeer-radar-86c1994b9733.herokuapp.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-willwhyd-reindeerradar-4958ydmonhb.ws.codeinstitute-ide.net', '8000-willwhyd-reindeerradar-grbzno7xz8c.ws.codeinstitute-ide.net', 'reindeer-radar-86c1994b9733.herokuapp.com', '.herokuapp.com']
 
-CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net',
+CSRF_TRUSTED_ORIGINS = ['https://*.codeinstitute-ide.net', 
                         'https://*.herokuapp.com']
 
 
@@ -51,9 +51,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary',
     'journey',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -64,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'reindeer_radar.urls'
@@ -120,6 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
