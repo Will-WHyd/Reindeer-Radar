d4bc427e4cd3d0e75a24fdc2d9dbab8e7b2e47ef
Borys Shesterniak <1b4r1sdc@students.codeinstitute.net>
2024-12-18 16:54:38 +0000

    Updated the lists - padding and styling, fixed other small errors

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,23 @@
 function toggleStatus(url) {
     window.location.href = url;
 }
+
+
+// Function for adding numbers (total cost)
+
+function calculateTotal() {
+
+    const rows = document.querySelectorAll("#expenseTable tbody tr");
+    let totalCost = 0;
+
+    rows.forEach(row => {
+        const cost = parseFloat(row.querySelector(".cost").textContent) || 0;
+        totalCost += cost;
+    });
+
+    document.getElementById("totalCost").textContent = totalCost.toFixed(2);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    calculateTotal();
+});
