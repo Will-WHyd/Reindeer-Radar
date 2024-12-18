3ca8f9e18dd44e7c6dd8f9a905f5182700140ef6
William Waldron-Hayden <williamjwh@gmail.com>
2024-12-18 14:09:57 +0000

    added create view for household

--- journey/urls.py
+++ journey/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
 
+app_name = 'journey'
 
 urlpatterns = [
+    path('add_house/', views.add_house, name='add'),
     path('journey/', views.journey_list, name='journey'),
     path('', views.HomePage.as_view(), name='home'),
 ]
--- journey/views.py
+++ journey/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 from .models import Household, Kid
+from .forms import *
 
 
 # Create your views here.
@@ -16,6 +18,29 @@ class HomePage(TemplateView):
 
 def journey_list(request):
 
-    households = Household.objects.all()
+    household_list = Household.objects.all()
 
-    return render(request, 'journey/list.html', {"households": households})
+
+    return render(request, 'journey/list.html', {"household_list": household_list})
+
+
+def add_house(request):
+    """ 
+    Creates a new instance of Household via form
+    """ 
+    h_form = HouseholdForm()
+    
+    if request.method == 'POST':
+        h_form = HouseholdForm(request.POST)
+        if form.is_valid():
+            household = h_form.save(commit=False)
+            household.user = request.user
+            household.save()
+
+            messages.add_message(
+                request, messages.SUCCESS,
+                'New Destination Added! Christmas Joy inbound!'
+            )
+
+            return redirect('journey/list.html')
+        return render(request, 'journey/add_house.html', {'h_form': h_form})
